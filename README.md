47cfae5feb9891a18c4d32472e86af343fa4440b
Allyn Ralf Ledesma <allynralfledesma@MacBook-Pro.local>
2023-12-27 05:47:29 +0800

    feat: added new topics in Notes folder and updated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JS-zero-to-full-stack
+
+<div>
+<h3>Note: <br> txt files inside Note folder are just topics that I personally forgot or not kind of familiar or hard.</h3>
+</div>
--- test.js
+++ test.js
@@ -1,21 +1,6 @@
-/* TYPE CONVERSION
-
+/* 
 
 
 */
 
-let money = "50";
-console.log(typeof money); // string
-// typeof returns the data type of the passed in variable (or value)
 
-// convert STRING to INTEGER. 3 WAYS
-//money = parseInt(money);
-// console.log(typeof money); // number
-
-// 2nd way is using this:
-// money = +money;
-// console.log(typeof money); // number
-
-// 3rd way is using Number()
-money = Number(money);
-console.log(typeof money); // number
